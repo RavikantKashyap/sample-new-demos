d23b86bbffcba5239a2f5b8af4cc13dd5282386d
Ravikant <ravikantkashyap36@gmail.com>
2023-03-15 18:35:24 +0530

    sidebar mein courses add kiye

--- app.py
+++ app.py
@@ -4,3 +4,10 @@ st.text("aaj hum sikh rahe hai Git")
 st.write("pehla commit to ho gya ab dusra commit karte hai bhaisahab")
 
 st.header('ye wala 2nd header hai')
+
+st.sidebar.title("courses")
+st.sidebar.markdown("""
+-Home
+-About
+-Contact
+""")
